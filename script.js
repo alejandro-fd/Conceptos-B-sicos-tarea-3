955254ea573e035ad758eb0b83655bbd4725067b
alejandroperez <viejoalejo730@gmail.com>
2023-10-19 21:59:57 -0500

    conceptosB-tarea3

---
+++ script.js
@@ -0,0 +1,5 @@
+let nombre=prompt("ingrese su nombre");
+let horas=Number(prompt("ingrese horas trabajadas"));
+let base=Number(prompt("ingrese salario base"));
+let resultado=(horas*base);
+alert("Señ@r "+nombre+" usted trabajo "+horas+ " horas asi que su sueldo sera de: $"+resultado);
